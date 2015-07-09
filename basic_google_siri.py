eebccfbf517a87c7528bd5cb5e54394f0bf56819
arvin1209 <curiousme.avi@gmail.com>
2015-07-09 09:32:44 -0400

    Create basic_google_siri.py
    basic beginner google siri

---
+++ basic_google_siri.py
@@ -0,0 +1,12 @@
+#This should reply hello or 1 depending on what you type
+c=str(input("Type something to Pybuddy in quote marks"))
+b="hello"
+d="what is one times one"
+e="what is 1*1"
+f="what is 1 times 1"
+g="what is one*one"
+if c == b:
+    print("Hello to you too")
+
+if c==d or e or f or g:
+        print("1")
